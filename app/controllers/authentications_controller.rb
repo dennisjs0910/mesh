f002d92f6e6a4d210984697ee8ac7b93f70946b8
brandon <blee298@gmail.com>
2014-11-16 22:19:19 +0000

    twitteroauthobject

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,6 +1,9 @@
+require 'pry'
+
 class AuthenticationsController < ApplicationController
   def index
     @authentications = current_user.authentications if current_user
+    twitter
   end
 
   def create
@@ -9,11 +12,30 @@ class AuthenticationsController < ApplicationController
       # update the uid with the latest token
     # if the user doesn't have an authentication for the provider
       # create an authentication with the provider and uid
-    current_user.find_or_create_authentication(auth['provider'], auth['uid'])    
+    current_user.find_or_create_authentication(auth['provider'], auth['uid'], auth['credentials']['token'], auth['credentials']['secret'])   
+       
     # current_user.authentications.find_or_create_by_provider_and_uid(auth['provider'], auth['uid'])
     flash[:notice] = "AIYO IT WORKS"
+    twitter
+    # @client = Twitter::REST::Client.new do |config|
+    #   binding.pry
+    #   config.consumer_key        = 'F24Pacf6CN7o1gH9E4turYnDz'
+    #   config.consumer_secret     = 'LoFVLjRIpKMdmq0hmoeslLSEgV8qCqdrZi6SvZdJH6lnUvIV9c'
+    #   config.access_token        = current_user.authentications.find_by(provider: 'twitter').token
+    #   config.access_token_secret = current_user.authentications.find_by(provider: 'twitter').secret
+
+    # end
     redirect_to authentications_url
   end
+
+  def twitter
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = 'F24Pacf6CN7o1gH9E4turYnDz'
+      config.consumer_secret     = 'LoFVLjRIpKMdmq0hmoeslLSEgV8qCqdrZi6SvZdJH6lnUvIV9c'
+      config.access_token        = current_user.authentications.find_by(provider: 'twitter').token
+      config.access_token_secret = current_user.authentications.find_by(provider: 'twitter').secret
+    end
+  end 
 
   def destroy
     @authentication = current_user.authentications.find(params[:id])
@@ -21,4 +43,16 @@ class AuthenticationsController < ApplicationController
     flash[:notice] = "Successfully destroyed authentication."
     redirect_to authentications_url
   end
+
+  # def show
+  #   @oauth ||= Twitter::OAuth.new(AppConfig['consumer']['token'], AppConfig['consumer']['secret'])
+  #   @oauth.authorize_from_request(session['rtoken'], session['rsecret'], params[:oauth_verifier])
+
+  #   session['rtoken'] = nil
+  #   session['rsecret'] = nil
+  #   session['atoken'] = @oauth.access_token.token
+  #   session['asecret'] = @oauth.access_token.secret
+  #   redirect_path = session['admin'] ? admin_tweets_path : root_path
+  #   redirect_to redirect_path
+  # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,18 +10,22 @@ class User < ActiveRecord::Base
   validates_presence_of :email
   validates_uniqueness_of :email
 
-  def find_or_create_authentication(provider, token)
+  def find_or_create_authentication(provider, uid, token, secret)
     provider_auth = authentications.find_by(provider: provider)
 
     if !provider_auth
       authen = Authentication.new
       authen.provider = provider
-      authen.uid = token
+      authen.uid = uid
       authen.user_id = self.id
+      authen.secret = secret
+      authen.token = token
       authen.save!
-    elsif provider_auth && provider_auth.uid != token
+    elsif provider_auth && provider_auth.token != token
       puts "Hello world"
-      provider_auth.uid = token
+      provider_auth.token = token
+      provider_auth.uid = uid
+      provider_auth.secret = secret
       provider_auth.save!
     end
     
