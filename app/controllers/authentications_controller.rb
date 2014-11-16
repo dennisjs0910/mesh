740b8cbe473926d15f935373355436be52b107a2
dennisjs0910 <dennis.joonsuk.yi@gmail.com>
2014-11-16 22:16:30 +0000

    rough idea

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -9,11 +9,13 @@ class AuthenticationsController < ApplicationController
       # update the uid with the latest token
     # if the user doesn't have an authentication for the provider
       # create an authentication with the provider and uid
-    current_user.find_or_create_authentication(auth['provider'], auth['uid'])    
+    current_user.find_or_create_authentication(auth['provider'], auth['uid'], auth['credentials']['token'], auth['credentials']['secret'])    
     # current_user.authentications.find_or_create_by_provider_and_uid(auth['provider'], auth['uid'])
     flash[:notice] = "AIYO IT WORKS"
     redirect_to authentications_url
   end
+
+
 
   def destroy
     @authentication = current_user.authentications.find(params[:id])
---
+++ config/initializers/twitter_credentials.rb
@@ -0,0 +1,10 @@
+# client = Twitter::REST::Client.new do |config|
+#   config.consumer_key        = "F24Pacf6CN7o1gH9E4turYnDz"
+#   config.consumer_secret     = "LoFVLjRIpKMdmq0hmoeslLSEgV8qCqdrZi6SvZdJH6lnUvIV9c"
+#   config.access_token        = "2898841538-17QHJT3hgI8tqVHIsZGCRHJGgGfPTGg1ttWEJv0"
+#   config.access_token_secret = "NUacrDTnZXsuGBXSxlfYdpEDx3KQjNWroEMBOEBKw8JUX"
+
+#  end
+
+#  client.status(27558893223
+
