c86290eaf17b5011a4fa886307ae73091680e88c
dennisjs0910 <dennis.joonsuk.yi@gmail.com>
2014-11-19 01:16:23 +0000

    no changes

--- app/controllers/soundcloud_controller.rb
+++ app/controllers/soundcloud_controller.rb
@@ -2,7 +2,7 @@ class SoundcloudController < ApplicationController
   def index
     soundcloud_user = SoundcloudUser.new(current_user)
     binding.pry
-    @sounds = soundcloud_user.tracks
+    @sounds = soundcloud_user
     # :limit => 10, :order => 'hotness'
   end
 end
