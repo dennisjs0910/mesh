b5c3e57880dcf8e7246dcae150cb4426986a43a0
brandon <blee298@gmail.com>
2014-11-20 23:36:51 +0000

    .

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -3,10 +3,10 @@ class InstagramController < ApplicationController
     client = Instagram.client(:access_token => current_user.instagram_authentication.token)
     @instas = client.media_popular
     @insta_newsfeed = client.user_media_feed
-    # @search_by_tag = client.tag_search(search)
-    # @insta_like = client.like_media("#{params[:id]}")
-    # @insta_unlike = client.unlike_media("#{params[:id]}")
-  
+    @search_by_tag = client.tag_search(params[:search])
+    @insta_like = client.like_media(params[:id])
+    @insta_unlike = client.unlike_media(params[:id])
+    binding.pry
   end
 
   ###each one of these functions may need to be put into a different controller after we have decided on 
