d8a05ad7e1ad8c87e9a649ee02920abdba6f5d1c
dennisjs0910 <dennis.joonsuk.yi@gmail.com>
2014-11-20 23:44:53 +0000

    refactored twitter controller

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -1,29 +1,25 @@
 class TwitterController < ApplicationController
-  def index
-    @twitter_user = TwitterUser.new(current_user)
-    @timeline = @twitter_user.get_timeline
+
+  def index 
+    @timeline = twitter_user.get_timeline
   end
 
   def create
-    @twitter_user = TwitterUser.new(current_user)
-    @tweet = @twitter_user.tweet(params[:message])
+    @tweet = twitter_user.tweet(params[:message])
     redirect_to twitter_index_path
   end
 
   def re_tweet
-    @twitter_user = TwitterUser.new(current_user)
-    @retweet = @twitter_user.retweet(params[:id])
+    @retweet = twitter_user.retweet(params[:id])
     redirect_to twitter_index_path
   end 
 
   def home
-    @twitter_user = TwitterUser.new(current_user)
-    @twitter_user.get_home_timeline
+    twitter_user.get_home_timeline
   end
 
   def search
-    @twitter_user = TwitterUser.new(current_user)
-    @tweet = @twitter_user.search_tweets(params[:search])
+    @tweet = twitter_user.search_tweets(params[:search])
   end
 
 end
