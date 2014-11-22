416e00d598ecd2f97b22a8b313ca45769ec8dac5
brandon <blee298@gmail.com>
2014-11-22 01:09:27 +0000

    added instagram usernames

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -1,7 +1,7 @@
 class InstagramController < ApplicationController
   def index
-    
-    @instas = instagram_user.media_popular
+    client = Instagram.client(:access_token => current_user.instagram_authentication.token)
+    @instas = client.media_popular
     @insta_newsfeed = client.user_media_feed
     
     # @insta_like = client.like_media
@@ -12,7 +12,9 @@ class InstagramController < ApplicationController
     client = Instagram.client(:access_token => current_user.instagram_authentication.token)
     search = client.tag_search(params[:search])
     @media_item = client.tag_recent_media(search[0].name)
+    
     # @media_item.images[0].thumbnail.url
+    # @media_item = client.tag_recent_media(search[0].name)[0].user.username
   end 
 
   ###each one of these functions may need to be put into a different controller after we have decided on 
