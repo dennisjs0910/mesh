e5e5946ca87ab690897729c894a9fd2793bb5c7b
brandon <blee298@gmail.com>
2014-11-25 02:30:12 +0000

    reddit omniauth available

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :instagram, 'b3d6f75cc55946d193ad37d0395356fa','9c592f5c2e004a48893c7636b6e585e4', :scope => 'basic likes'
   provider :facebook, '383367571788257', '61a0a4c317af1acca95e5add55472334'
   provider :soundcloud, '31b35595e00750bf40213344343e81f1', '39613d98981c264e14dcbd8dead5896f'
-  provider :reddit, 'vOsw4t8ygEHS2Q', 'HjbN35t4g7oOPW0wCp5hBRadTmc', :scope => 'read'
+  provider :reddit, 'vOsw4t8ygEHS2Q', 'HjbN35t4g7oOPW0wCp5hBRadTmc', :scope => 'identity, read'
+
 end
