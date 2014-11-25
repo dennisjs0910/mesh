db3a8da13fdd5a53e26a6d98ff5c3c9172459f24
dennisjs0910 <dennis.joonsuk.yi@gmail.com>
2014-11-25 04:47:35 +0000

    iframe soundcloud working

--- app/controllers/soundcloud_controller.rb
+++ app/controllers/soundcloud_controller.rb
@@ -4,8 +4,6 @@ class SoundcloudController < ApplicationController
     @sounds = soundcloud_user.hot_tracks
 
     @sounds.each do |sound|
-      # 'http://soundcloud.com/forss/flickermood'
-      #puts sound.permalink_url
       sound[:iframe_html] = soundcloud_user.embed_music(sound.permalink_url)
     end
   end
