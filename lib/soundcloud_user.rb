5d848440951442465a9e248a29e804e0a3461e7a
dennisjs0910 <dennis.joonsuk.yi@gmail.com>
2014-11-27 04:56:17 +0000

    re-deploy

--- lib/soundcloud_user.rb
+++ lib/soundcloud_user.rb
@@ -10,7 +10,7 @@ class SoundcloudUser
     @client = Soundcloud.new({
       :client_id     => '31b35595e00750bf40213344343e81f1',
       :client_secret => '39613d98981c264e14dcbd8dead5896f',
-      :redirect_uri  => '127.0.0.1:3000/soundcloud/callback'
+      :redirect_uri  => 'meshapp.herokuapp.com/soundcloud/callback'
       })
   end
 
